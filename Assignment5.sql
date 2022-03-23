00d7a7616257d1bfa0f850afd1dab2b051e382a2
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-03-23 23:12:44 +0530

    Update Assignment5.sql

--- Assignment5.sql
+++ Assignment5.sql
@@ -6,29 +6,37 @@ Branch: MCA
 */
 
 No1.
-1) select name, phone_no, cust_no from CUSTOMER natural join DEPOSITOR where DEPOSITOR.account_no = 'A0004';
+a) select name, phone_no, cust_no from CUSTOMER natural join DEPOSITOR where DEPOSITOR.account_no = 'A0004';
 
    select name, phone_no, cust_no from CUSTOMER where cust_no in (select cust_no from DEPOSITOR where account_no = 'A0004');
 
-2) select amount, branch_code from LOAN natural join CUSTOMER where CUSTOMER.name = 'YASH SARAF';
+b) select amount, branch_code from LOAN natural join CUSTOMER where CUSTOMER.name = 'YASH SARAF';
 
    select amount, branch_code from LOAN where cust_no in (select cust_no from CUSTOMER where name = 'YASH SARAF');
 
-3) select CUSTOMER.name from CUSTOMER left join LOAN on CUSTOMER.cust_no = LOAN.cust_no where LOAN.cust_no is NULL;
+c) select CUSTOMER.name from CUSTOMER left join LOAN on CUSTOMER.cust_no = LOAN.cust_no where LOAN.cust_no is NULL;
 
   select name from CUSTOMER where cust_no not in (select cust_no from LOAN);
 
-4) select account_no, balance from ACCOUNT natural join DEPOSITOR where DEPOSITOR.cust_no = 'C0010';
+d) select account_no, balance from ACCOUNT natural join DEPOSITOR where DEPOSITOR.cust_no = 'C0010';
 
   select account_no, balance from ACCOUNT where account_no in (select account_no from DEPOSITOR where cust_no = 'C0010');
 
-5) select distinct CUSTOMER.city from CUSTOMER self join CUSTOMER on CUSTOMER.name = 'ASLESHA TIWARI';
+e) select distinct CUSTOMER.city from CUSTOMER self join CUSTOMER on CUSTOMER.name = 'ASLESHA TIWARI';
 
   select city from CUSTOMER where cust_no in (select cust_no from CUSTOMER where name = 'ASLESHA TIWARI');
 
-6) select type, branch_code from ACCOUNT natural join DEPOSITOR where DEPOSITOR.cust_no = 'C0002';
+f) select loan_no, branch_code,amount from loan natural join CUSTOMER where CUSTOMER.name = 'ANKITA SINGH';
 
-  select type, branch_code from ACCOUNT where account_no in (select account_no from DEPOSITOR where cust_no = 'C0002');
+  select loan_no, branch_code,amount from loan where cust_no in(select cust_no from customer where name = 'ANKITA SINGH');
+
+g) select branch_name, branch_city from branch natural join customer where customer.name = 'ABHIJIT MISHRA';
+
+select branch_name, branch_city from branch where branch_city in(select branch_city from customer where name = 'ABHIJIT MISHRA');
+
+h) create table account_type (select account_no,type from account);
+
+i) insert into account_type(select account_no,type from account where balance<50000);
 
 7) select account_no, balance from ACCOUNT where type = 'SB';
 
